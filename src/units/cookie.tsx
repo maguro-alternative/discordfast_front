db4390c52c38e39ed93e4eed820d976a8b6b5d97
ygo <ka.ra.si.naporitan@gmail.com>
2023-07-27 18:07:54 +0900

    cookie操作のラッパー作成

---
+++ src/units/cookie.tsx
@@ -0,0 +1,36 @@
+
+// Cookieをセットするカスタムフック
+export function useSetCookie() {
+    const setCookie = (name:string, value:string | number | boolean, days:number) => {
+        const expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + days);
+        const cookieValue = `${name}=${encodeURIComponent(value)}; expires=${expirationDate.toUTCString()}; path=/`;
+        document.cookie = cookieValue;
+    };
+    return setCookie;
+}
+
+// Cookieを取得するカスタムフック
+export function useGetCookie() {
+    const getCookie = (name:string) => {
+        const cookieName = `${name}=`;
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            //console.log(cookies[i])
+            let cookie = cookies[i].trim();
+            if (cookie.indexOf(cookieName) === 0) {
+                return decodeURIComponent(cookie.substring(cookieName.length));
+            }
+        }
+        return null;
+    };
+    return getCookie;
+}
+
+// Cookieを削除するカスタムフック
+export function useDeleteCookie() {
+    const deleteCookie = (name:string) => {
+        document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    };
+    return deleteCookie;
+}
