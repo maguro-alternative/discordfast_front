26bd8ad8283bc43302c87ae1ecf786aab7689d56
ygo <ka.ra.si.naporitan@gmail.com>
2023-07-29 09:38:37 +0900

    新たに型定義

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,3 +1,5 @@
 import discordCallbackResponse from "./typejsons/discordtype.json"
+import discordOAuthResponse from "./typejsons/discordoauthtoken.json"
 
 export type DiscordCallbackResponse = typeof discordCallbackResponse
+export type DiscordOAuthResponse = typeof discordOAuthResponse
