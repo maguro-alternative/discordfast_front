e37318c823c4cf977b4279edbd51bec665305255
ygo <ka.ra.si.naporitan@gmail.com>
2023-08-23 09:37:26 +0900

    新たな型宣言追加

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,7 +1,9 @@
 import discordCallbackResponse from "./typejsons/discordtype.json"
 import discordOAuthResponse from "./typejsons/discordoauthtoken.json"
 import discordGuilds from './typejsons/discordguild.json'
+import discordGuildID from './typejsons/discordguildid.json'
 
 export type DiscordCallbackResponse = typeof discordCallbackResponse
 export type DiscordOAuthResponse = typeof discordOAuthResponse
 export type DiscordGuilds = typeof discordGuilds
+export type DiscordGuildID = typeof discordGuildID
