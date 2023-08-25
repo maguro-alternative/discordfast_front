7d9d7d86cf8229946d63225c19833e855823171e
ygo <ka.ra.si.naporitan@gmail.com>
2023-08-25 09:07:11 +0900

    フォームの定義追加

--- src/page/guild/admin/index.tsx
+++ src/page/guild/admin/index.tsx
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from "react";
+import Select from "react-select";
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 import { DiscordAdmin } from '../../../store';
+
+interface AdminFormData {
+    guild_id                    : number;
+    line_permission             : number;
+    line_user_id_permission     : number[];
+    line_role_id_permission     : number[];
+    line_bot_permission         : number;
+    line_bot_user_id_permission : number[];
+    line_bot_role_id_permission : number[];
+    vc_permission               : number;
+    vc_user_id_permission       : number[];
+    vc_role_id_permission       : number[];
+    webhook_permission          : number;
+    webhook_user_id_permission  : number[];
+    webhook_role_id_permission  : number[];
+}
 
 const Admin = () => {
     const { id } = useParams(); // パラメータを取得
@@ -33,6 +50,8 @@ const Admin = () => {
             ignore = true;
         };
     },[]);
+
+    adminData?.guildMembers
 
     return(
         <></>
