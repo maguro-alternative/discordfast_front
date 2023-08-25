ff287b18a8b6a741330cef15fffbdf024cffc9a0
ygo <ka.ra.si.naporitan@gmail.com>
2023-08-25 17:39:06 +0900

    登録されているユーザとロールを取得

--- src/page/guild/admin/index.tsx
+++ src/page/guild/admin/index.tsx
@@ -4,7 +4,12 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 import { DiscordAdmin } from '../../../store';
-import { UserIdComprehension,UserIdIndexComprehension } from "../../../units/dictComprehension";
+import {
+    UserIdComprehension,
+    UserIdIndexComprehension,
+    RoleIdComprehension,
+    RoleIdIndexComprehension
+} from "../../../units/dictComprehension";
 
 interface AdminFormData {
     guild_id                    : number;
@@ -27,20 +32,32 @@ const Admin = () => {
 
     const [adminData, setAdminData] = useState<DiscordAdmin>();
 
+    const guildMember = adminData && adminData.guildMembers !== undefined ? adminData.guildMembers : [];
+    const guildRole = adminData && adminData.guildRoles !== undefined ? adminData.guildRoles : [];
+
+    const lineUserIds = adminData && adminData.lineUserIdPermission !== undefined ? adminData.lineUserIdPermission : [];
+    const lineRoleIds = adminData && adminData.lineRoleIdPermission !== undefined ? adminData.lineRoleIdPermission : [];
+    const lineBotUserIds = adminData && adminData.lineBotUserIdPermission !== undefined ? adminData.lineBotUserIdPermission : [];
+    const lineBotRoleIds = adminData && adminData.lineBotRoleIdPermission !== undefined ? adminData.lineBotRoleIdPermission : [];
+    const vcUserIds = adminData && adminData.vcUserIdPermission !== undefined ? adminData.vcUserIdPermission : [];
+    const vcRoleIds = adminData && adminData.vcRoleIdPermission !== undefined ? adminData.vcRoleIdPermission : [];
+    const webhookUserIds = adminData && adminData.webhookUserIdPermission !== undefined ? adminData.webhookUserIdPermission : [];
+    const webhookRoleIds = adminData && adminData.webhookRoleIdPermission !== undefined ? adminData.webhookRoleIdPermission : [];
+
     const [formAdminData, setAdminFormData] = useState<AdminFormData>({
         guild_id                    :Number(id),
-        line_permission             :adminData?.linePermission || 8,
-        line_user_id_permission     :adminData?.lineUserIdPermission || [],
-        line_role_id_permission     :adminData?.lineRoleIdPermission || [],
-        line_bot_permission         :adminData?.lineBotPermission || 8,
-        line_bot_user_id_permission :adminData?.lineBotUserIdPermission || [],
-        line_bot_role_id_permission :adminData?.lineBotRoleIdPermission || [],
-        vc_permission               :adminData?.vcPermission || 8,
-        vc_user_id_permission       :adminData?.vcUserIdPermission || [],
-        vc_role_id_permission       :adminData?.vcRoleIdPermission || [],
-        webhook_permission          :adminData?.webhookPermission || 8,
-        webhook_user_id_permission  :adminData?.webhookUserIdPermission || [],
-        webhook_role_id_permission  :adminData?.webhookRoleIdPermission || []
+        line_permission             :adminData && adminData.linePermission !== undefined ? adminData.linePermission : 8,
+        line_user_id_permission     :lineUserIds,
+        line_role_id_permission     :lineRoleIds,
+        line_bot_permission         :adminData && adminData.lineBotPermission !== undefined ? adminData.lineBotPermission : 8,
+        line_bot_user_id_permission :lineBotUserIds,
+        line_bot_role_id_permission :lineBotRoleIds,
+        vc_permission               :adminData && adminData.vcPermission !== undefined ? adminData.vcPermission : 8,
+        vc_user_id_permission       :vcUserIds,
+        vc_role_id_permission       :vcRoleIds,
+        webhook_permission          :adminData && adminData.webhookPermission !== undefined ? adminData.webhookPermission : 8,
+        webhook_user_id_permission  :webhookUserIds,
+        webhook_role_id_permission  :webhookRoleIds
     });
 
     const SERVER_BASE_URL = process.env.REACT_APP_SERVER_URL
@@ -68,10 +85,40 @@ const Admin = () => {
         };
     },[]);
 
-    const userIdSelect = UserIdComprehension(adminData?.guildMembers || []);
-    const lineUserIdSelect = UserIdIndexComprehension(
-        adminData?.lineUserIdPermission || [],
-        adminData?.guildMembers || []
+    const userIdSelect = UserIdComprehension(guildMember);
+    const lineUserIdSelected = UserIdIndexComprehension(
+        lineUserIds,
+        guildMember
+    );
+    const lineBotUserIdSelected = UserIdIndexComprehension(
+        lineBotUserIds,
+        guildMember
+    );
+    const vcUserIdSelected = UserIdIndexComprehension(
+        vcUserIds,
+        guildMember
+    );
+    const webhookUserIdSelect = UserIdIndexComprehension(
+        webhookUserIds,
+        guildMember
+    );
+
+    const roleIdSelect = RoleIdComprehension(guildRole);
+    const lineRoleIDSelected = RoleIdIndexComprehension(
+        lineRoleIds,
+        guildRole
+    );
+    const lineBotRoleIDSelected = RoleIdIndexComprehension(
+        lineBotRoleIds,
+        guildRole
+    );
+    const vcRoleIDSelected = RoleIdIndexComprehension(
+        vcRoleIds,
+        guildRole
+    );
+    const webhookRoleIDSelected = RoleIdIndexComprehension(
+        webhookRoleIds,
+        guildRole
     );
 
     return(
--- src/units/dictComprehension.ts
+++ src/units/dictComprehension.ts
@@ -49,3 +49,53 @@ export function UserIdIndexComprehension(
 
     return optionList;
 }
+
+
+export function RoleIdComprehension(
+    roleList:DiscordAdmin["guildRoles"]
+){
+    let optionDict: {
+        value:number,
+        label:string
+    };
+    let optionList: {
+        value:number,
+        label:string
+    }[] = [];
+
+    roleList.forEach(role => {
+        optionDict = {
+            value:role.roleId,
+            label:role.roleName
+        }
+        optionList.push(optionDict);
+    });
+
+    return optionList;
+}
+
+export function RoleIdIndexComprehension(
+    roleIdList:number[],
+    roleList:DiscordAdmin["guildRoles"]
+){
+    let optionDict: {
+        value:number,
+        label:string
+    };
+    let optionList: {
+        value:number,
+        label:string
+    }[] = [];
+
+    roleList.forEach(role => {
+        if (role.roleId in roleIdList){
+            optionDict = {
+                value:role.roleId,
+                label:role.roleName
+            }
+            optionList.push(optionDict);
+        }
+    });
+
+    return optionList;
+}
