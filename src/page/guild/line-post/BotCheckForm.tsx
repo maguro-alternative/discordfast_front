cd8c0dad31e08e27ce8184febd81078253595e08
ygo <ka.ra.si.naporitan@gmail.com>
2023-08-30 14:42:50 +0900

    コンポーネント化用のファイル作成

---
+++ src/page/guild/line-post/BotCheckForm.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useMemo } from "react";
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import Select,{ MultiValue } from "react-select";
+
+import { DiscordLinePost,SelectOption,Channel } from '../../../store';
+
+const BotCheck = () => {}
+
+export default BotCheck;
