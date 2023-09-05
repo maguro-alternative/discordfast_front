8f646c48027371cc19ce98a114ecc947072fec82
ygo <ka.ra.si.naporitan@gmail.com>
2023-09-05 11:53:58 +0900

    複数のwebhook作成に対応

--- src/page/guild/webhook/CreateNewWebhook.tsx
+++ src/page/guild/webhook/CreateNewWebhook.tsx
@@ -64,183 +64,194 @@ const CreateNewWebhookSelection:React.FC<CreateNewWebhookSelectionProps> = ({
             <summary>
                 <strong>新規作成</strong>
             </summary>
-            {newCreateWebhook.map((newWebhook) => (
-                <div key={newWebhook.uuid}>
-                    <h6>WebHook</h6>
-                    <Select
-                        options={webhookSelects}
-                        onChange={(value) => {
-                            if(value){
-                                handleNewWebhookChange(
-                                    value,
+            <ul>
+                <button
+                    type="button"
+                    onClick={() => newWebhookSetting()}
+                >項目追加</button>
+                {newCreateWebhook.map((newWebhook) => (
+                    <details key={newWebhook.uuid}>
+                        <summary>
+                            <strong>新規作成</strong>
+                        </summary>
+                        <ul>
+                            <h6>WebHook</h6>
+                            <Select
+                                options={webhookSelects}
+                                onChange={(value) => {
+                                    if(value){
+                                        handleNewWebhookChange(
+                                            value,
+                                            newWebhook.uuid
+                                        )
+                                    }
+                                }}
+                            ></Select>
+
+                            <h6>サブスクリプションタイプ(例:twitter,niconico)</h6>
+                            <input type="text" id={`subscType${newWebhook.uuid}`} name="subscType_1"/>
+
+                            <h6>サブスクリプションid(例:twitter:@ユーザ名(@は含まない),niconico:/user/userId)</h6>
+                            <input type="text" id={`subscId${newWebhook.uuid}`} name="subscId_1"/>
+
+                            <h6>メンションするロールの選択</h6>
+                            <Select
+                                options={webhookRoles}
+                                onChange={(value) => {
+                                    if(value){
+                                        handleNewWebhookRoleChange(
+                                            [...value],
+                                            newWebhook.uuid
+                                        )
+                                    }
+                                }}
+                                isMulti // trueに
+                            ></Select>
+
+                            <h6>メンションするメンバーの選択</h6>
+                            <Select
+                                options={webhookUsers}
+                                onChange={(value) => {
+                                    if(value){
+                                        handleNewWebhookUserChange(
+                                            [...value],
+                                            newWebhook.uuid
+                                        )
+                                    }
+                                }}
+                                isMulti // trueに
+                            ></Select>
+
+                            <h3>ワードカスタム(niconicoには反映されません)</h3>
+                            <h6>キーワードOR検索(いずれかの言葉が含まれている場合、送信)</h6>
+                            {newWebhook.search_or_word.map((sOrWord,index) => (
+                                <div key={`${sOrWord}${index}`}>
+                                    <label>検索条件:{index + 1}</label>
+                                    <input
+                                        id={`searchOrWord${newWebhook.uuid}`}
+                                        name={`${index}`}
+                                        type="text"
+                                        value={sOrWord}
+                                        onChange={handleNewWebhookInputChange}
+                                    ></input>
+                                </div>
+                            ))}
+                            <button
+                                type="button"
+                                onClick={() => handleNewWebhookInputArray(
+                                    "searchOrWord",
                                     newWebhook.uuid
-                                )
-                            }
-                        }}
-                    ></Select>
+                                )}
+                            >条件追加</button>
 
-                    <h6>サブスクリプションタイプ(例:twitter,niconico)</h6>
-                    <input type="text" id={`subscType${newWebhook.uuid}`} name="subscType_1"/>
-
-                    <h6>サブスクリプションid(例:twitter:@ユーザ名(@は含まない),niconico:/user/userId)</h6>
-                    <input type="text" id={`subscId${newWebhook.uuid}`} name="subscId_1"/>
-
-                    <h6>メンションするロールの選択</h6>
-                    <Select
-                        options={webhookRoles}
-                        onChange={(value) => {
-                            if(value){
-                                handleNewWebhookRoleChange(
-                                    [...value],
+                            <h6>キーワードAND検索(すべての単語が含まれている場合、送信)</h6>
+                            {newWebhook.search_and_word.map((sAndWord,index) => (
+                                <div key={`${sAndWord}${index}`}>
+                                    <label>検索条件:{index + 1}</label>
+                                    <input
+                                        id={`searchAndWord${newWebhook.uuid}`}
+                                        name={`${index}`}
+                                        type="text"
+                                        value={sAndWord}
+                                        onChange={handleNewWebhookInputChange}
+                                    ></input>
+                                </div>
+                            ))}
+                            <button
+                                type="button"
+                                onClick={() => handleNewWebhookInputArray(
+                                    "searchAndWord",
                                     newWebhook.uuid
-                                )
-                            }
-                        }}
-                        isMulti // trueに
-                    ></Select>
+                                )}
+                            >条件追加</button>
 
-                    <h6>メンションするメンバーの選択</h6>
-                    <Select
-                        options={webhookUsers}
-                        onChange={(value) => {
-                            if(value){
-                                handleNewWebhookUserChange(
-                                    [...value],
+                            <h6>NGワードOR検索(いずれかの言葉が含まれている場合、送信しない)</h6>
+                            {newWebhook.ng_or_word.map((nOrWord,index) => (
+                                <div key={`${nOrWord}${index}`}>
+                                    <label>検索条件:{index + 1}</label>
+                                    <input
+                                        id={`ngOrWord${newWebhook.uuid}`}
+                                        name={`${index}`}
+                                        type="text"
+                                        value={nOrWord}
+                                        onChange={handleNewWebhookInputChange}
+                                    ></input>
+                                </div>
+                            ))}
+                            <button
+                                type="button"
+                                onClick={() => handleNewWebhookInputArray(
+                                    "ngOrWord",
                                     newWebhook.uuid
-                                )
-                            }
-                        }}
-                        isMulti // trueに
-                    ></Select>
+                                )}
+                            >条件追加</button>
 
-                    <h3>ワードカスタム(niconicoには反映されません)</h3>
-                    <h6>キーワードOR検索(いずれかの言葉が含まれている場合、送信)</h6>
-                    {newWebhook.search_or_word.map((sOrWord,index) => (
-                        <div key={`${sOrWord}${index}`}>
-                            <label>検索条件:{index + 1}</label>
-                            <input
-                                id={`searchOrWord${newWebhook.uuid}`}
-                                name={`${index}`}
-                                type="text"
-                                value={sOrWord}
-                                onChange={handleNewWebhookInputChange}
-                            ></input>
-                        </div>
-                    ))}
-                    <button
-                        type="button"
-                        onClick={() => handleNewWebhookInputArray(
-                            "searchOrWord",
-                            newWebhook.uuid
-                        )}
-                    >条件追加</button>
+                            <h6>NGワードAND検索(すべての単語が含まれている場合、送信しない)</h6>
+                            {newWebhook.ng_and_word.map((nAndWord,index) => (
+                                <div key={`${nAndWord}${index}`}>
+                                    <label>検索条件:{index + 1}</label>
+                                    <input
+                                        id={`ngAndWord${newWebhook.uuid}`}
+                                        name={`${index}`}
+                                        type="text"
+                                        value={nAndWord}
+                                        onChange={handleNewWebhookInputChange}
+                                    ></input>
+                                </div>
+                            ))}
+                            <button
+                                type="button"
+                                onClick={() => handleNewWebhookInputArray(
+                                    "ngAndWord",
+                                    newWebhook.uuid
+                                )}
+                            >条件追加</button>
 
-                    <h6>キーワードAND検索(すべての単語が含まれている場合、送信)</h6>
-                    {newWebhook.search_and_word.map((sAndWord,index) => (
-                        <div key={`${sAndWord}${index}`}>
-                            <label>検索条件:{index + 1}</label>
-                            <input
-                                id={`searchAndWord${newWebhook.uuid}`}
-                                name={`${index}`}
-                                type="text"
-                                value={sAndWord}
-                                onChange={handleNewWebhookInputChange}
-                            ></input>
-                        </div>
-                    ))}
-                    <button
-                        type="button"
-                        onClick={() => handleNewWebhookInputArray(
-                            "searchAndWord",
-                            newWebhook.uuid
-                        )}
-                    >条件追加</button>
+                            <h6>メンションOR検索(いずれかの言葉が含まれている場合、メンションを付けて送信)</h6>
+                            {newWebhook.mention_or_word.map((mOrWord,index) => (
+                                <div key={`${mOrWord}${index}`}>
+                                    <label>検索条件:{index + 1}</label>
+                                    <input
+                                        id={`mentionOrWord${newWebhook.uuid}`}
+                                        name={`${index}`}
+                                        type="text"
+                                        value={mOrWord}
+                                        onChange={handleNewWebhookInputChange}
+                                    ></input>
+                                </div>
+                            ))}
+                            <button
+                                type="button"
+                                onClick={() => handleNewWebhookInputArray(
+                                    "mentionOrWord",
+                                    newWebhook.uuid
+                                )}
+                            >条件追加</button>
 
-                    <h6>NGワードOR検索(いずれかの言葉が含まれている場合、送信しない)</h6>
-                    {newWebhook.ng_or_word.map((nOrWord,index) => (
-                        <div key={`${nOrWord}${index}`}>
-                            <label>検索条件:{index + 1}</label>
-                            <input
-                                id={`ngOrWord${newWebhook.uuid}`}
-                                name={`${index}`}
-                                type="text"
-                                value={nOrWord}
-                                onChange={handleNewWebhookInputChange}
-                            ></input>
-                        </div>
-                    ))}
-                    <button
-                        type="button"
-                        onClick={() => handleNewWebhookInputArray(
-                            "ngOrWord",
-                            newWebhook.uuid
-                        )}
-                    >条件追加</button>
-
-                    <h6>NGワードAND検索(すべての単語が含まれている場合、送信しない)</h6>
-                    {newWebhook.ng_and_word.map((nAndWord,index) => (
-                        <div key={`${nAndWord}${index}`}>
-                            <label>検索条件:{index + 1}</label>
-                            <input
-                                id={`ngAndWord${newWebhook.uuid}`}
-                                name={`${index}`}
-                                type="text"
-                                value={nAndWord}
-                                onChange={handleNewWebhookInputChange}
-                            ></input>
-                        </div>
-                    ))}
-                    <button
-                        type="button"
-                        onClick={() => handleNewWebhookInputArray(
-                            "ngAndWord",
-                            newWebhook.uuid
-                        )}
-                    >条件追加</button>
-
-                    <h6>メンションOR検索(いずれかの言葉が含まれている場合、メンションを付けて送信)</h6>
-                    {newWebhook.mention_or_word.map((mOrWord,index) => (
-                        <div key={`${mOrWord}${index}`}>
-                            <label>検索条件:{index + 1}</label>
-                            <input
-                                id={`mentionOrWord${newWebhook.uuid}`}
-                                name={`${index}`}
-                                type="text"
-                                value={mOrWord}
-                                onChange={handleNewWebhookInputChange}
-                            ></input>
-                        </div>
-                    ))}
-                    <button
-                        type="button"
-                        onClick={() => handleNewWebhookInputArray(
-                            "mentionOrWord",
-                            newWebhook.uuid
-                        )}
-                    >条件追加</button>
-
-                    <h6>メンションAND検索(すべての単語が含まれている場合、メンションを付けて送信)</h6>
-                    {newWebhook.mention_and_word.map((mAndWord,index) => (
-                        <div key={`${mAndWord}${index}`}>
-                            <label>検索条件:{index + 1}</label>
-                            <input
-                                id={`mentionAndWord${newWebhook.uuid}`}
-                                name={`${index}`}
-                                type="text"
-                                value={mAndWord}
-                                onChange={handleNewWebhookInputChange}
-                            ></input>
-                        </div>
-                    ))}
-                    <button
-                        type="button"
-                        onClick={() => handleNewWebhookInputArray(
-                            "mentionAndWord",
-                            newWebhook.uuid
-                        )}
-                    >条件追加</button>
-                </div>
-            ))}
+                            <h6>メンションAND検索(すべての単語が含まれている場合、メンションを付けて送信)</h6>
+                            {newWebhook.mention_and_word.map((mAndWord,index) => (
+                                <div key={`${mAndWord}${index}`}>
+                                    <label>検索条件:{index + 1}</label>
+                                    <input
+                                        id={`mentionAndWord${newWebhook.uuid}`}
+                                        name={`${index}`}
+                                        type="text"
+                                        value={mAndWord}
+                                        onChange={handleNewWebhookInputChange}
+                                    ></input>
+                                </div>
+                            ))}
+                            <button
+                                type="button"
+                                onClick={() => handleNewWebhookInputArray(
+                                    "mentionAndWord",
+                                    newWebhook.uuid
+                                )}
+                            >条件追加</button>
+                        </ul>
+                    </details>
+                ))}
+            </ul>
         </details>
     );
 }
