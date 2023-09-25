b27a565e59ffd8f4e41074d5c4d5feca28018def
ygo <ka.ra.si.naporitan@gmail.com>
2023-09-25 13:38:09 +0900

    背景色をヘッダーと合わせる

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return(
         <footer>
-            <div style={{textAlign:"center"}}>
+            <div style={{textAlign:"center",backgroundColor: '#5865f2'}}>
                 <p>Copyright &copy; maguro Inc. All right reserves.</p>
                 <p>Powered by React 18.2.0 &amp; FastAPI 0.88.0 &amp; starlette 0.22.0</p>
             </div>
