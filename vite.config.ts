fffb01772c875a7f283275b6b24141f4a6fc809e
ygo <ka.ra.si.naporitan@gmail.com>
2023-10-08 08:21:13 +0900

    設定変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    root: './',
+    //root: './',
     server: {
         open: true, // 自動でブラウザを開く
         port: 3000, // ポート番号
@@ -11,6 +12,11 @@ export default defineConfig({
     build: {
         outDir: 'dist', // ビルドの出力先ディレクトリ
         sourcemap: false, // ソースマップの出力
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+            },
+        },
     },
     publicDir: 'public', // 静的ファイルのディレクトリ
     plugins: [react()],
