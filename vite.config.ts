7bbea0e06401e120860207c611aaf375180a8225
ygo <ka.ra.si.naporitan@gmail.com>
2023-10-07 22:54:21 +0900

    いろいろ追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    root: './',
     server: {
         open: true, // 自動でブラウザを開く
         port: 3000, // ポート番号
         host: true, // ネットワーク上のIPアドレスを表示
     },
     build: {
-        outDir: 'build', // ビルドの出力先ディレクトリ
+        outDir: 'dist', // ビルドの出力先ディレクトリ
         sourcemap: false, // ソースマップの出力
     },
+    publicDir: 'public', // 静的ファイルのディレクトリ
     plugins: [react()],
 });
